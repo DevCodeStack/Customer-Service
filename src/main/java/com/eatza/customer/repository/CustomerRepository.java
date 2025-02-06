a9c743b3f6fa7f77ae629abe9d527462178ceda7
Deepak Biswal <deepak.biswal211621@gmail.com>
2025-02-06 02:54:39 +0000

    Database engine updated (#3)

--- src/main/java/com/eatza/customer/repository/CustomerRepository.java
+++ src/main/java/com/eatza/customer/repository/CustomerRepository.java
@@ -11,20 +11,20 @@ import com.eatza.customer.model.Customer;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	
 	@Modifying
-	@Query(nativeQuery = true, value = "Update customers set mail_id = ?2, update_timestamp = CURRENT_TIMESTAMP() where id = ?1")
+	@Query(nativeQuery = true, value = "Update eatza.customers set mail_id = ?2, update_timestamp = CURRENT_TIMESTAMP where id = ?1")
 	public Integer updateMailId(Long id, String mailId);
 	
 	@Modifying
-	@Query(nativeQuery = true, value = "Update customers set password = ?2, update_timestamp = CURRENT_TIMESTAMP(), password_last_update = CURRENT_TIMESTAMP() where id = ?1")
+	@Query(nativeQuery = true, value = "Update eatza.customers set password = ?2, update_timestamp = CURRENT_TIMESTAMP, password_last_update = CURRENT_TIMESTAMP where id = ?1")
 	public Integer updatePassword(Long id, String password);
 	
 	@Modifying
-	@Query(nativeQuery = true, value = "Update customers set active = ?2, update_timestamp = CURRENT_TIMESTAMP() where id = ?1")
+	@Query(nativeQuery = true, value = "Update eatza.customers set active = ?2, update_timestamp = CURRENT_TIMESTAMP where id = ?1")
 	public Integer updateUserActive(Long id, char active);
 	
-	@Query(nativeQuery = true, value = "select * from customers where username = ?1")
+	@Query(nativeQuery = true, value = "select * from eatza.customers where username = ?1")
 	public Optional<Customer> findByUsername(String username);
 	
-	@Query(nativeQuery = true, value = "select * from customers where id = ?1")
+	@Query(nativeQuery = true, value = "select * from eatza.customers where id = ?1")
 	public Optional<Customer> findById(Long id);
 }
